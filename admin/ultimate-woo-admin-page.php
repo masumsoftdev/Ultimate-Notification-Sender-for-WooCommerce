c24ced5e50e433848ef8604fb7aef583188dd77e
Masum Billah <office.hellomasum@gmail.com>
2024-03-02 17:11:29 +0600

    updated readme

--- admin/ultimate-woo-admin-page.php
+++ admin/ultimate-woo-admin-page.php
@@ -34,17 +34,17 @@ class Uwns_Admin {
     public function settings_page() {
         ?>
         <div class="wrap">
-            <h1><?php _e('Ultimate Woo Notifications Settings', 'ultimate-woo-notification-sender'); ?></h1>
+            <h1><?php esc_html_e('Ultimate Woo Notifications Settings', 'ultimate-woo-notification-sender'); ?></h1>
             <form method="post" action="options.php">
             <?php settings_fields('ultimate_woo_sender_options'); ?>
                 <?php $options = get_option('uwns_settings', array()); ?>
                 <table class="form-table">
                     <tr valign="top">
-                            <th scope="row"><?php _e('Telegram API Token', 'ultimate-woo-notification-sender'); ?></th>
+                            <th scope="row"><?php esc_html_e('Telegram API Token', 'ultimate-woo-notification-sender'); ?></th>
                             <td><input type="text" name="uwns_settings[telegram_api_token]" value="<?php echo esc_attr($options['telegram_api_token'] ?? ''); ?>" /></td>
                     </tr>
                     <tr valign="top">
-                        <th scope="row"><?php _e('Telegram Chat ID', 'ultimate-woo-notification-sender'); ?></th>
+                        <th scope="row"><?php esc_html_e('Telegram Chat ID', 'ultimate-woo-notification-sender'); ?></th>
                         <td><input type="text" name="uwns_settings[telegram_chat_id]" value="<?php echo esc_attr($options['telegram_chat_id'] ?? ''); ?>" /></td>
                     </tr>
                 </table>
